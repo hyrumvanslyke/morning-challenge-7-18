be50af84681dd7f7fdb0af07091caf9c49204540
Hyrum Van Slyke <hyrumevanslyke@gmail.com>
2023-07-18 11:21:55 -0400

    Updated morning challenge for 7/18

--- addToZero.js
+++ addToZero.js
@@ -2,7 +2,7 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
-function addsToZero (x){
+function addsToZero (x){ // my take on the problem
     for(let i = 0; i < x.length; i++){
         for(let j =0; j < x.length; j++){
             if(x[i] === -x[j]){
@@ -13,3 +13,16 @@ function addsToZero (x){
     }
 }
 addsToZero(array)
+
+let value = false // correct answer according to teacher
+for(i = 0; i < array.length; i++){
+    for(j = 0; j < array.length; j++){
+        if(i !== j) {
+            if(array[i] + array[j] === 0) {
+                value = true
+            }
+        }
+    }
+}
+
+console.log(value)
